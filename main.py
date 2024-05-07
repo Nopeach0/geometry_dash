9899340a407857c727f48a741ee0ba36909542d4
Behdad <Behdadkh123@gmail.com>
2024-05-07 14:58:59 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
+import pygame
+pygame.init()
 def hello_world():
     print(f"hello BK")
 
 
 hello_world()
 
+<<<<<<< Updated upstream
 print(hello_world)
 
 
 hello hello 
 
 
+=======
+pygame.
+>>>>>>> Stashed changes
